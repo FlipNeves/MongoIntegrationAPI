e6fe3fa654e8891b812699862f00c993e81b50d6
Filipe Neves <filipe.neves.bg@gmail.com>
2026-03-19 12:36:17 -0300

    Removendo fluxo em cima de [Infected]

--- MongoIntegrationAPI/Domain/IInfectedRepository.cs
+++
@@ -1,8 +0,0 @@
-namespace MongoIntegrationAPI.Domain
-{
-    public interface IInfectedRepository
-    {
-        Task Add(Infected infected);
-        Task<IEnumerable<Infected>> GetAll();
-    }
-}
