34c0b07899f4886a4fada491764ed4c1cf05f45f
Filipe Neves <filipe.neves.bg@gmail.com>
2026-03-19 12:49:23 -0300

    Voltando o GenericRepository

---
+++ MongoIntegrationAPI/Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,11 @@
+namespace MongoIntegrationAPI.Domain.Interfaces
+{
+    public interface IGenericRepository<TDocument> where TDocument : class
+    {
+        Task AddAsync(TDocument document);
+        Task<TDocument?> GetByIdAsync(string id);
+        Task<IEnumerable<TDocument>> GetAllAsync();
+        Task ReplaceAsync(string id, TDocument document);
+        Task DeleteAsync(string id);
+    }
+}
---
+++ MongoIntegrationAPI/Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,51 @@
+using System.ComponentModel;
+using MongoIntegrationAPI.Domain.Entities;
+using MongoIntegrationAPI.Domain.Enums;
+using MongoIntegrationAPI.Domain.Interfaces;
+
+namespace MongoIntegrationAPI.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> GetAllCategories()
+        {
+            var categories = new List<Category>();
+            var enumValues = Enum.GetValues(typeof(CategoryType)).Cast<CategoryType>();
+
+            foreach (var value in enumValues)
+            {
+                categories.Add(new Category
+                {
+                    Name = value.ToString(),
+                    Description = GetEnumDescription(value)
+                });
+            }
+
+            return categories;
+        }
+
+        public Category? GetCategoryByType(int categoryId)
+        {
+            if (!Enum.IsDefined(typeof(CategoryType), categoryId))
+                return null;
+
+            var categoryType = (CategoryType)categoryId;
+            
+            return new Category
+            {
+                Name = categoryType.ToString(),
+                Description = GetEnumDescription(categoryType)
+            };
+        }
+
+        private string GetEnumDescription(Enum value)
+        {
+            var fieldInfo = value.GetType().GetField(value.ToString());
+            if (fieldInfo == null) return string.Empty;
+            
+            var attributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return attributes.Length > 0 ? attributes[0].Description : value.ToString();
+        }
+    }
+}
